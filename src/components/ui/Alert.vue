b8a102cc4281d9f7bd77035be9a615c864ec70df
Roberton003 <roberto.m0010@gmail.com>
2025-09-21 13:42:27 -0300

    Criar componentes UI adicionais: Modal e Alert

---
+++ src/components/ui/Alert.vue
@@ -0,0 +1,111 @@
+<template>
+  <div :class="alertClass" role="alert">
+    <div class="flex">
+      <div class="flex-shrink-0">
+        <i :class="iconClass"></i>
+      </div>
+      <div class="ml-3">
+        <h3 class="text-sm font-medium" :class="titleClass">
+          <slot name="title" />
+        </h3>
+        <div class="mt-2 text-sm" :class="messageClass">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: {
+    variant: {
+      type: String,
+      default: 'info'
+    }
+  },
+  computed: {
+    alertClass() {
+      let baseClass = 'rounded-md p-4 '
+      
+      switch (this.variant) {
+        case 'success':
+          baseClass += 'bg-green-50 '
+          break
+        case 'error':
+          baseClass += 'bg-red-50 '
+          break
+        case 'warning':
+          baseClass += 'bg-yellow-50 '
+          break
+        case 'info':
+        default:
+          baseClass += 'bg-blue-50 '
+      }
+      
+      return baseClass
+    },
+    iconClass() {
+      let baseClass = 'h-5 w-5 '
+      
+      switch (this.variant) {
+        case 'success':
+          baseClass += 'text-green-400 '
+          break
+        case 'error':
+          baseClass += 'text-red-400 '
+          break
+        case 'warning':
+          baseClass += 'text-yellow-400 '
+          break
+        case 'info':
+        default:
+          baseClass += 'text-blue-400 '
+      }
+      
+      return baseClass
+    },
+    titleClass() {
+      let baseClass = ''
+      
+      switch (this.variant) {
+        case 'success':
+          baseClass += 'text-green-800 '
+          break
+        case 'error':
+          baseClass += 'text-red-800 '
+          break
+        case 'warning':
+          baseClass += 'text-yellow-800 '
+          break
+        case 'info':
+        default:
+          baseClass += 'text-blue-800 '
+      }
+      
+      return baseClass
+    },
+    messageClass() {
+      let baseClass = ''
+      
+      switch (this.variant) {
+        case 'success':
+          baseClass += 'text-green-700 '
+          break
+        case 'error':
+          baseClass += 'text-red-700 '
+          break
+        case 'warning':
+          baseClass += 'text-yellow-700 '
+          break
+        case 'info':
+        default:
+          baseClass += 'text-blue-700 '
+      }
+      
+      return baseClass
+    }
+  }
+}
+</script>
---
+++ src/components/ui/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div 
+    v-if="visible" 
+    class="fixed inset-0 z-50 overflow-y-auto"
+    @click="closeModal"
+  >
+    <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-dark opacity-75"></div>
+      </div>
+
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+      <div 
+        class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        @click.stop
+      >
+        <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="sm:flex sm:items-start">
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+              <h3 class="text-lg leading-6 font-medium text-dark">
+                <slot name="header" />
+              </h3>
+              <div class="mt-2">
+                <slot />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <slot name="footer" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['close'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
