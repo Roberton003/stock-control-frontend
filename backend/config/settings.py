fe174a4bf52c4488294c52b8112aadf4ee9bc60e
Roberton003 <rob3rto88@gmail.com>
2025-09-23 10:45:46 -0300

    feat(backend): Configura CORS para comunicação com o frontend
    - Instala a biblioteca django-cors-headers.
    
    - Adiciona e configura o middleware de CORS no settings.py.
    
    - Permite requisições da origem do frontend (http://localhost:3000) através da configuração CORS_ALLOWED_ORIGINS.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 # Application definition
 
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'inventory',
     'django_celery_beat',
     'django_celery_results',
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
